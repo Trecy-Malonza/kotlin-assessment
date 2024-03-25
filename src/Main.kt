d71c36f4357a0bbe89e0a44e325b1b120b2833e3
Trecy-Malonza <trecymalonza@gmail.com>
2024-03-25 08:24:28 +0300

    added nnew file

---
+++ src/Main.kt
@@ -0,0 +1,50 @@
+fun main() {
+   occurrences("Barnie bakes brown bagels and buns")
+
+    values(30,59,67)
+    volume(3.14159,34,4/3)
+    ispalindrome("racecar")
+}
+fun occurrences(text:String){
+    println(text.slice(1..6))
+    println(text.slice(8..12))
+    println(text.slice(14..18))
+    println(text.slice(20..29))
+    println(text.slice(31..33))
+}
+
+fun values(num1:Int,num2:Int,num3:Int):Int{
+    var values = arrayOf(num1,num2,num3)
+    var sum =num1 +num2 +num3
+    return sum
+    var count = sum.countOneBits()
+    return count
+        var average = sum/3
+    return average
+
+
+}
+
+fun volume(pie:Double,radius:Int,num:Int){
+  var  volume = 4/3 *3.14159*34*34*34
+    println(volume)
+
+}
+
+
+
+
+fun ispalindrome(word: String) :Boolean{
+    if("word.ispalindrome){
+        return true}
+
+else{
+    return false
+}
+}
+
+
+
+
+
+
